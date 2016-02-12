44e0c1cae68dead49ba48237408751a7c1bde920
Mike <student@epicodus.com>
2016-02-12 10:54:33 -0800

    finish up bbd testing and functions for Pizza Constructor

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,41 @@ function Topping(topping) {
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
+  this.price;
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  return this.toppings.push(topping);
+}
+
+Pizza.prototype.pizzPrice = function() {
+  var cost = 0;
+
+  if (this.size === "Small") {
+    cost = 10.00;
+  } else if (this.size === "Medium") {
+    cost = 12.00;
+  } else if (this.size === "Large") {
+    cost = 15.00;
+  } else if (this.size === "WagonWheel") {
+    cost = 17.00;
+  }
+  return this.price = cost + this.toppingsPrice();
+}
+
+Pizza.prototype.toppingsPrice = function() {
+  var size = this.size;
+  var toppings = this.toppings.length;
+  var toppingCost = 0;
+
+  if (size === "Small" || size === "Medium") {
+    toppingCost = 0.75;
+  } else if (size === "Large") {
+    toppingCost = 1;
+  } else if (size === "WagonWheel") {
+    toppingCost = 1.25;
+  }
+  return toppings * toppingCost;
 }
 
 function Order() {
--- spec/specs.js
+++ spec/specs.js
@@ -11,6 +11,93 @@ describe('Pizza', function() {
     expect(testPizza.size).to.equal("Large");
     expect(testPizza.toppings).to.eql([]);
   });
+
+  it("Generates price for pizza no toppings", function() {
+    var smallPizza = new Pizza("Small");
+    expect(smallPizza.pizzPrice()).to.eql(10);
+    var mediumPizza = new Pizza("Medium");
+    expect(mediumPizza.pizzPrice()).to.eql(12);
+    var largePizza = new Pizza("Large");
+    expect(largePizza.pizzPrice()).to.eql(15);
+    var WagonWheel = new Pizza("WagonWheel");
+    expect(WagonWheel.pizzPrice()).to.eql(17);
+  });
+
+  it("Adds a topping to the pizza", function() {
+    var testPizza = new Pizza("Large");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    expect(testPizza.toppings[0].topping).to.eql("pepperoni");
+  });
+
+  it("Adds a topping to the pizza", function() {
+    var testPizza = new Pizza("Large");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    expect(testPizza.toppings[0].topping).to.eql("pepperoni");
+    expect(testPizza.toppings[1].topping).to.eql("olives");
+  });
+
+  it("creates topping price for large pizza, one dollar per topping", function() {
+    var testPizza = new Pizza("Large");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    expect(testPizza.toppingsPrice()).to.eql(2)
+  });
+
+  it("creates topping price for a medium pizza, 75 cents per topping", function() {
+    var testPizza = new Pizza("Medium");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    expect(testPizza.toppingsPrice()).to.eql(1.50)
+  });
+
+  it("creates topping price for a small pizza, 75 cents per topping", function() {
+    var testPizza = new Pizza("Small");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    expect(testPizza.toppingsPrice()).to.eql(1.50)
+  });
+
+  it("creates topping price for a WagonWheel Pizza, 1.25 per topping", function() {
+    var testPizza = new Pizza("WagonWheel");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    expect(testPizza.toppingsPrice()).to.eql(2.50)
+  });
+
+  it("Generates price for small pizza with 2 toppings", function() {
+    var testPizza = new Pizza("Small");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    expect(testPizza.pizzPrice()).to.eql(11.50);
+  });
+
+  it("Generates price for The WagonWheel with 4 toppings", function() {
+    var testPizza = new Pizza("WagonWheel");
+    var testTopping = new Topping("pepperoni");
+    testPizza.addTopping(testTopping);
+    var testTopping2 = new Topping("olives");
+    testPizza.addTopping(testTopping2);
+    var testTopping3 = new Topping("salami");
+    testPizza.addTopping(testTopping3);
+    var testTopping4 = new Topping("pineapple");
+    testPizza.addTopping(testTopping4);
+    expect(testPizza.pizzPrice()).to.eql(22.00);
+    console.log(testPizza)
+  });
 });
 
 describe('Order', function() {
